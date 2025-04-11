66e77d495bc3ccf1e1dbb0305fb247151949ed0b
leila.gomez <leilagomezgarcia@gmail.com>
2025-04-11 09:29:05 +0200

    Estructura de carpetas

---
+++ src/test/java/com/develop/mic_prices/MicPricesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.develop.mic_prices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicPricesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
