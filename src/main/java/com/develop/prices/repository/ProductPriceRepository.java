44cd50bd05a1ade27e9a2f43312970107c923a86
leila.gomez <leilagomezgarcia@gmail.com>
2025-04-16 17:31:22 +0200

    Filtro con Specifications (a medias)

--- src/main/java/com/develop/prices/repository/ProductPriceRepository.java
+++ src/main/java/com/develop/prices/repository/ProductPriceRepository.java
@@ -1,11 +1,12 @@
 package com.develop.prices.repository;
 
 import com.develop.prices.model.ProductPriceModel;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Optional;
 
 
@@ -14,5 +15,6 @@ public interface ProductPriceRepository extends JpaRepository<ProductPriceModel,
     Optional<ProductPriceModel> findByShop_ShopIdAndProduct_ProductId(Integer shopId, Integer productId);
 
 
-
+    @Query("select p from ProductPriceModel p join fetch p.product prod where LOWER(prod.name) LIKE LOWER(CONCAT('%', :name, '%')) or p.price >= :priceMin or p.price < :priceMax")
+    List<ProductPriceModel> foo(String name, BigDecimal priceMin, BigDecimal priceMax);
 }
---
+++ src/main/java/com/develop/prices/repository/ShopsSpecification.java
@@ -0,0 +1,29 @@
+package com.develop.prices.repository;
+
+import com.develop.prices.model.ShopModel;
+import com.develop.prices.model.dto.ShopDTO;
+import org.springframework.data.jpa.domain.Specification;
+
+public class ShopsSpecification {
+
+    public static Specification<ShopModel> findByCountry(String country){
+        return (root, query, criteriaBuilder) -> {
+            return criteriaBuilder.like(
+                    criteriaBuilder.lower(root.get("country")),"%" + country.toLowerCase() + "%");
+        };
+    }
+
+    public static Specification<ShopModel> findByCity(String city){
+        return (root, query, criteriaBuilder) -> {
+            return criteriaBuilder.like(
+                    criteriaBuilder.lower(root.get("city")),"%" + city.toLowerCase() + "%");
+        };
+    }
+
+    public static Specification<ShopModel> findByAddress(String address){
+        return (root, query, criteriaBuilder) -> {
+            return criteriaBuilder.like(
+                    criteriaBuilder.lower(root.get("address")),"%" + address.toLowerCase() + "%");
+        };
+    }
+}
