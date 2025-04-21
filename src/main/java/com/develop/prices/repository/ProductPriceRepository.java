562fa2ebaed47ab62c96e694d40cc84e2a52d222
Samupindo <samipindo22@gmail.com>
2025-04-21 08:39:29 +0200

    Comprobación de todos los métodos

--- src/main/java/com/develop/prices/repository/ProductPriceRepository.java
+++ src/main/java/com/develop/prices/repository/ProductPriceRepository.java
@@ -3,9 +3,11 @@ package com.develop.prices.repository;
 import com.develop.prices.model.ProductPriceModel;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Optional;
 
 
@@ -13,6 +15,7 @@ public interface ProductPriceRepository extends JpaRepository<ProductPriceModel,
     Optional<ProductPriceModel> findByPrice(BigDecimal precio);
     Optional<ProductPriceModel> findByShop_ShopIdAndProduct_ProductId(Integer shopId, Integer productId);
 
-
+    @Query("select p from ProductPriceModel p join fetch p.product prod where LOWER(prod.name) LIKE LOWER(CONCAT('%', :name, '%')) or p.price >= :priceMin or p.price < :priceMax")
+    List<ProductPriceModel> foo(String name, BigDecimal priceMin, BigDecimal priceMax);
 
 }
