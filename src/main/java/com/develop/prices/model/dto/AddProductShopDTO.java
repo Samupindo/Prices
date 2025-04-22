b491c780a7ad41f95efac228aad474a6b6042a08
Samupindo <samipindo22@gmail.com>
2025-04-22 08:53:03 +0200

    Comprobaciones y traducciones

--- src/main/java/com/develop/prices/model/dto/AddProductShopDTO.java
+++ src/main/java/com/develop/prices/model/dto/AddProductShopDTO.java
@@ -1,13 +1,19 @@
 package com.develop.prices.model.dto;
 
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
 
 public class AddProductShopDTO implements  Serializable { //este DTO y el de ProductPricePatchDTO son lo mismo
 
-    @NotBlank(message = "El campo price no debe ir vacío")
+    @Positive(message = "The price must be positive")
+    @DecimalMin(value="0.01", message = "The minimum price must be 0.01")
+    @DecimalMax(value = "999999999.99", message = "The maximum price must be 999999999")
+    @NotBlank(message = "The field price cannot be empty")
     private BigDecimal price;
 
     public AddProductShopDTO() {
