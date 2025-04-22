4dd0b7d86dfbd4a0c858049293ecdad3bbbf1a14
Samupindo <samipindo22@gmail.com>
2025-04-22 16:57:11 +0200

    Filtros de Customers

--- src/main/java/com/develop/prices/controller/CustomerController.java
+++ src/main/java/com/develop/prices/controller/CustomerController.java
@@ -6,14 +6,18 @@ import com.develop.prices.model.dto.CustomerDTO;
 import com.develop.prices.dto.PageResponse;
 import com.develop.prices.repository.CustomerRepository;
 import com.develop.prices.repository.PurchaseRepository;
+import com.develop.prices.specification.CustomerSpecification;
+import com.develop.prices.specification.ProductPriceSpecification;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -33,16 +37,41 @@ public class CustomerController {
     }
 
     @GetMapping("")
-    public ResponseEntity<PageResponse> getAllCustomers(
+    public ResponseEntity<PageResponse<CustomerDTO>> getCustomersWithFilters(
+            @RequestParam(required = false) Integer customerId,
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) Integer phone,
+            @RequestParam(required = false) String email,
         @PageableDefault(sort = "customerId", direction = Sort.Direction.ASC) Pageable pageable) {
 
-            Page<CustomerModel> customerPage = customerRepository.findAll(pageable);
+
+            Specification<CustomerModel> spec = Specification.where(null);
+
+        if (customerId != null) {
+            spec = spec.and(CustomerSpecification.hasCustomerId(customerId));
+        }
+
+        if (name != null && !name.isBlank()) {
+            spec = spec.and(CustomerSpecification.hasName(name));
+        }
+
+        if (phone != null) {
+            spec = spec.and(CustomerSpecification.hasPhone(phone));
+        }
+
+        if (email != null && !email.isBlank()) {
+            spec = spec.and(CustomerSpecification.hasEmail(email));
+        }
+
+
+
+            Page<CustomerModel> customerPage = customerRepository.findAll(spec,pageable);
             List<CustomerDTO> customerDTOList = customerPage.getContent()
                     .stream()
                     .map(customerMapper::customerModelToCustomerDTO)
                     .toList();
 
-            PageResponse pageResponse = new PageResponse(
+            PageResponse<CustomerDTO> pageResponse = new PageResponse<>(
                     customerDTOList,
                     customerPage.getTotalElements(),
                     customerPage.getTotalPages()
