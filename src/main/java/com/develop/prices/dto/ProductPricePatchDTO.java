a1e2fa1ef41de22e30cadc3716324bce4792a252
Samupindo <samipindo22@gmail.com>
2025-04-22 13:13:01 +0200

    Correciones

--- src/main/java/com/develop/prices/dto/ProductPricePatchDTO.java
+++ src/main/java/com/develop/prices/dto/ProductPricePatchDTO.java
@@ -1,13 +1,14 @@
 package com.develop.prices.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
 
 public class ProductPricePatchDTO implements Serializable {
 
-    @NotBlank
+    @NotNull
     private BigDecimal price;
 
     public ProductPricePatchDTO() {
