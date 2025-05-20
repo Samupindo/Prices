9a8943a8fa7326619dd31dbeff82fab00b713936
Samupindo <samipindo22@gmail.com>
2025-05-20 11:06:43 +0200

    Test Integral Purchase Listo

---
+++ mic-prices-boot/src/test/java/PurchaseControllerTestIT.java
@@ -0,0 +1,227 @@
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
+@SpringBootTest(classes = com.develop.prices.MicPricesApplication.class)
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+@Transactional
+class PurchaseControllerTestIT {
+
+  @Autowired private MockMvc mockMvc;
+
+  private Integer validId;
+  private Integer invalidId;
+  private String validPurchaseJson;
+  private String invalidPurchaseJson;
+
+  @BeforeEach
+  void setUp() {
+    validId = 4;
+    invalidId = 999;
+
+    validPurchaseJson =
+        """
+          {
+          "customerId": 1
+           }
+        """;
+    invalidPurchaseJson =
+        """
+          {
+          "customerId": 99
+           }
+        """;
+  }
+
+  @Test
+  public void getPurchasesWithFilters() throws Exception {
+    mockMvc.perform(MockMvcRequestBuilders.get("/purchases")).andDo(print());
+    mockMvc
+        .perform(
+            MockMvcRequestBuilders.get("/purchases")
+                .param("customerId", "1")
+                .param("totalPriceMax", "100.00")
+                .param("totalPriceMin", "0.00")
+                .param("page", "0")
+                .param("size", "10")
+                .param("sort", "purchaseId,asc"))
+        .andExpect(status().isOk())
+        .andDo(print())
+        .andExpect(jsonPath("$.content", notNullValue()))
+        .andExpect(jsonPath("$.content[0].purchaseId", notNullValue()))
+        .andExpect(jsonPath("$.content[0].customer.customerId", is(1)))
+        .andExpect(jsonPath("$.content[0].totalPrice", greaterThanOrEqualTo(7.90)))
+        .andExpect(jsonPath("$.content.length()", greaterThanOrEqualTo(1)))
+        .andExpect(jsonPath("$.totalElements", greaterThanOrEqualTo(1)));
+  }
+
+  @Test
+  public void getPurchasesWithOutFilters() throws Exception {
+    mockMvc
+        .perform(
+            MockMvcRequestBuilders.get("/purchases")
+                .param("page", "0")
+                .param("size", "20")
+                .param("sort", "purchaseId,asc"))
+        .andExpect(status().isOk())
+        .andDo(MockMvcResultHandlers.print())
+        .andExpect(jsonPath("$.content.length()", greaterThanOrEqualTo(1)))
+        .andExpect(jsonPath("$.totalElements", greaterThanOrEqualTo(1)));
+  }
+
+  @Test
+  public void getPurchasesWithInvalidFilters() throws Exception {
+    mockMvc
+        .perform(
+            MockMvcRequestBuilders.get("/purchases")
+                .param("customerId", "999")
+                .param("totalPriceMax", "100.00")
+                .param("totalPriceMin", "0.00"))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.content").isEmpty())
+        .andExpect(jsonPath("$.totalElements").value(0));
+  }
+
+  @Test
+  public void getPurchaseById_success() throws Exception {
+    mockMvc
+        .perform(MockMvcRequestBuilders.get("/purchases/" + validId))
+        .andExpect(status().isOk())
+        .andDo(MockMvcResultHandlers.print())
+        .andExpect(jsonPath("$.purchaseId", is(4)))
+        .andExpect(jsonPath("$.customer.customerId", is(3)))
+        .andExpect(jsonPath("$.totalPrice", greaterThanOrEqualTo(5.50)));
+  }
+
+  @Test
+  public void getPurchaseById_notFound() throws Exception {
+    mockMvc
+        .perform(MockMvcRequestBuilders.get("/purchases/" + invalidId))
+        .andExpect(status().isNotFound());
+  }
+
+  @Test
+  public void postPurchase_success() throws Exception {
+    mockMvc
+        .perform(
+            MockMvcRequestBuilders.post("/purchases")
+                .content(validPurchaseJson)
+                .contentType(MediaType.APPLICATION_JSON))
+        .andDo(MockMvcResultHandlers.print())
+        .andExpect(status().isCreated())
+        .andExpect(jsonPath("$.purchaseId", notNullValue()))
+        .andExpect(jsonPath("$.customer.customerId", is(1)))
+        .andExpect(jsonPath("$.totalPrice", greaterThanOrEqualTo(0)))
+        .andExpect(jsonPath("$.shopping").value(true));
+  }
+
+  @Test
+  public void postPurchase_invalidCustomerId() throws Exception {
+    mockMvc
+        .perform(
+            MockMvcRequestBuilders.post("/purchases")
+                .content(invalidPurchaseJson)
+                .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isNotFound());
+  }
+
+  @Test
+  public void addProductPurchase_success() throws Exception {
+    mockMvc
+        .perform(MockMvcRequestBuilders.post("/purchases/" + validId + "/productInShop/" + validId))
+        .andDo(MockMvcResultHandlers.print())
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.purchaseId", notNullValue()))
+        .andExpect(jsonPath("$.customer.customerId", is(3)))
+        .andExpect(jsonPath("$.totalPrice", greaterThanOrEqualTo(7.90)))
+        .andExpect(jsonPath("$.shopping").value(true));
+  }
+
+  @Test
+  public void addProductPurchase_invalidPurchaseId() throws Exception {
+    mockMvc
+        .perform(
+            MockMvcRequestBuilders.post("/purchases/" + invalidId + "/productInShop/" + validId))
+        .andExpect(status().isNotFound());
+  }
+
+  @Test
+  public void addProductPurchase_invalidProductInShopId() throws Exception {
+    mockMvc
+        .perform(
+            MockMvcRequestBuilders.post("/purchases/" + validId + "/productInShop/" + invalidId))
+        .andExpect(status().isNotFound());
+  }
+
+  @Test
+  public void patchFinishPurchase_success() throws Exception {
+    mockMvc
+        .perform(MockMvcRequestBuilders.patch("/purchases/" + validId + "/finish"))
+        .andDo(MockMvcResultHandlers.print())
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.purchaseId", notNullValue()))
+        .andExpect(jsonPath("$.customer.customerId", is(3)))
+        .andExpect(jsonPath("$.shopping").value(false));
+  }
+
+  @Test
+  public void patchFinishPurchase_invalidPurchaseId() throws Exception {
+    mockMvc
+        .perform(MockMvcRequestBuilders.patch("/purchases/" + invalidId + "/finish"))
+        .andExpect(status().isNotFound());
+  }
+
+  @Test
+  public void deletePurchase_success() throws Exception {
+    mockMvc
+        .perform(MockMvcRequestBuilders.delete("/purchases/" + validId))
+        .andDo(MockMvcResultHandlers.print())
+        .andExpect(status().isOk());
+  }
+
+  @Test
+  public void deletePurchase_invalidPurchaseId() throws Exception {
+    mockMvc
+        .perform(MockMvcRequestBuilders.delete("/purchases/" + invalidId))
+        .andExpect(status().isNotFound());
+  }
+
+  @Test
+  public void deletePurchase_productoInPurchase() throws Exception {
+    mockMvc
+        .perform(
+            MockMvcRequestBuilders.delete("/purchases/" + validId + "/productInShop/" + validId))
+        .andExpect(status().isOk());
+  }
+
+  @Test
+  public void deletePurchase_productoInPurchase_invalidPurchaseId() throws Exception {
+    mockMvc
+        .perform(
+            MockMvcRequestBuilders.delete("/purchases/" + invalidId + "/productInShop/" + validId))
+        .andExpect(status().isNotFound());
+  }
+
+  @Test
+  public void deletePurchase_productoInPurchase_invalidProductInShopId() throws Exception {
+    mockMvc
+        .perform(
+            MockMvcRequestBuilders.delete("/purchases/" + validId + "/productInShop/" + invalidId))
+        .andExpect(status().isNotFound());
+  }
+}
