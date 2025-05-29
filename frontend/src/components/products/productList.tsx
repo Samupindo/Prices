309ca928ead0192483c70674cfbb12e6431757de
Samupindo <samipindo22@gmail.com>
2025-05-29 16:43:58 +0200

    Botón buscar productId

--- frontend/src/components/products/productList.tsx
+++ frontend/src/components/products/productList.tsx
@@ -1,14 +1,18 @@
+import { useState } from "react";
 import type { ProductWithShopsDto } from "../../types/shops";
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 
 interface ProductListProps {
     products: ProductWithShopsDto[];
     totalPages: number;
 }
 
+
+
 export const ProductList = ({ products, totalPages }: ProductListProps) => {
     const cellPadding = "px-6 py-4";
-
+    const navigate = useNavigate();
+    const [findId, setFindId] = useState<string | null>(null);
     return (
         <div className="p-4">
             <div className="shadow-md sm:rounded-lg overflow-x-auto">
@@ -172,6 +176,10 @@ export const ProductList = ({ products, totalPages }: ProductListProps) => {
             </div>
             <button className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 mt-10 mr-70">
                 <Link to="/products-create" className="text-white">Añadir producto</Link>
+            </button>
+            <input  name="productId" type="number" placeholder="Find product by id" onChange={(e) => setFindId(e.target.value)} />
+            <button onClick={() => navigate(`/products/${findId}`)} className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-black bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 mt-10 mr-70">
+                Buscar
             </button>
         </div>
     );
