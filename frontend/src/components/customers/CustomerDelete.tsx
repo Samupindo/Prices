764aaf7dcd717e686b66c4ff3252ff2900906610
b.gonzalez <braisgonzalez737@gmail.com>
2025-06-03 13:02:47 +0200

    quedan funcionando customer, con paginacion, buscar por id

--- frontend/src/components/customers/CustomerDelete.tsx
+++ frontend/src/components/customers/CustomerDelete.tsx
@@ -66,7 +66,7 @@ export const CustomerDelete = () => {
                     <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
                         <button
                             onClick={handleDeleteCustomer}
-                            className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-black shadow-xs bg-red-800 hover:bg-red-500 ring-1 ring-red-300  sm:ml-3 sm:w-auto">Deactivate</button>
+                            className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-black shadow-xs bg-red-800 hover:bg-red-500 ring-1 ring-red-300  sm:ml-3 sm:w-auto">Delete</button>
                         <button
                             onClick={() => navigate("/customers")}
                             className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-xs ring-1 ring-gray-300 ring-inset hover:bg-gray-50 sm:mt-0 sm:w-auto">Cancel</button>
--- frontend/src/components/customers/customerList.tsx
+++ frontend/src/components/customers/customerList.tsx
@@ -90,6 +90,11 @@ const CustomerList = ({
                     Search
                 </button>
             </div>
+            <button
+                onClick={() => navigate("/customers/create")}
+                className="mt-4 bg-blue-500 text-black flex justify-left px-4 py-2 rounded hover:bg-blue-600 transition-colors duration-200">
+                Add Customer
+            </button>
             <div className="relative overflow-x-auto shadow-md mt-10 sm:rounded-lg">
                 <table className="min-w-full divide-y divide-gray-200">
                     <thead className="bg-black text-white sticky top-0">
@@ -100,20 +105,21 @@ const CustomerList = ({
                             <th className="px-20 py-3 text-left text-xs font-medium uppercase tracking-wider w-2/6">Actions</th>
                         </tr>
                     </thead>
-                    <tbody className="bg-white divide-y divide-gray-200 overflow-y-auto max-h-[50vh]">
+                    <tbody className="bg-gray-300 divide-y divide-gray-200 overflow-y-auto max-h-[50vh]">
                         {customers.map((customer: CustomerDto) => (
-                            <tr key={customer.customerId}
-                                className="hover:bg-gray-100 transition-colors duration-200">
+                            <tr onClick={() => navigate(`/customers/${customer.customerId}`)} key={customer.customerId}
+                                className="hover:bg-gray-100 transition-colors duration-200 cursor-pointer">
                                 <td className="px-6 py-4 whitespace-nowrap w-2/6">{customer.name}</td>
                                 <td className="px-6 py-4 whitespace-nowrap w-1/6">{customer.phone}</td>
                                 <td className="px-6 py-4 whitespace-nowrap w-2/6">{customer.email}</td>
                                 <td className="px-6 py-4 whitespace-nowrap w-2/6">
-                                    <button className="bg-yellow-600 hover:bg-yellow-800 ml-2" onClick={() => navigate(`/customers/${customer.customerId}/update`)}
+                                    <button className="bg-yellow-600 hover:bg-yellow-800 ml-2" onClick={(e) =>{e.stopPropagation(); navigate(`/customers/${customer.customerId}/edit`)}}
                                     >
                                         Edit
                                     </button>
-                                    <button className="bg-red-600 hover:bg-red-800 ml-2">
-                                        <a href={`/customers/delete/${customer.customerId}`}>Delete</a>
+                                    <button onClick={(e) => { e.stopPropagation(); navigate(`/customers/${customer.customerId}/delete`) }}
+                                    className="bg-red-600 hover:bg-red-800 ml-2">
+                                        Delete
                                     </button>
                                 </td>
                             </tr>
@@ -121,13 +127,7 @@ const CustomerList = ({
                     </tbody>
                 </table>
             </div>
-
-            <button
-                onClick={() => navigate("/customers-createCustomers")}
-                className="mt-4 bg-blue-500 text-black flex justify-left px-4 py-2 rounded hover:bg-blue-600 transition-colors duration-200">
-                Add Customer
-            </button>
-            <div className="flex items-center gap-6">
+            <div className="flex items-center gap-6 mt-10">
 
                 <PaginationDefault
                     currentPage={currentPage}
