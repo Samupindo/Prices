038377d248f73404d94d8bf2a15f6d8699a67240
b.gonzalez <braisgonzalez737@gmail.com>
2025-06-03 13:05:35 +0200

    pequeño cambio

--- frontend/src/components/customers/CustomerDetail.tsx
+++ frontend/src/components/customers/CustomerDetail.tsx
@@ -29,7 +29,7 @@ const CustomerDetail = ({ customer }: CustomerDetailProps) => {
                         Back to List
                     </button>
                     <button
-                        onClick={() => navigate(`/customers/${customer.customerId}/update`)}
+                        onClick={() => navigate(`/customers/${customer.customerId}/edit`)}
                         className="bg-blue-500 text-black px-4 py-2 rounded hover:bg-blue-600 transition-colors duration-200">
                         Edit
                     </button>
